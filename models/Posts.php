a4f202361901bf2e007caff5d3d6276f4e41cc42
James Siebert <js@media360.com.au>
2022-01-31 14:44:03 +1000

    Create Posts.php

---
+++ models/Posts.php
@@ -0,0 +1,48 @@
+<?php
+    class Post {
+        // Database
+        private $conn;
+        private $table = 'posts';
+
+        // Post properties
+        public $id;
+        public $category_id;
+        public $category_name;
+        public $title;
+        public $body;
+        public $author;
+        public $created_at;
+
+        // Constructor - Runs on instantiation of this class
+        public function __construct($db) {
+            $this->conn = $db;
+        }
+
+        // Get/Read Posts
+        public function read() {
+            $query = '
+                SELECT
+                    c.name as category_name,
+                    p.id,
+                    p.category_id,
+                    p.title,
+                    p.body,
+                    p.author,
+                    p.created_at
+                FROM
+                    ' . $this->table . ' p
+                LEFT JOIN
+                    categories c ON p.category_id = c.id
+                ORDER BY
+                    p.created_at DESC';
+
+            // Prepare Statement (PDO)
+            $stmt = $this->conn->prepare($query);
+
+            // Execute query
+            $stmt->execute();
+
+            return $stmt;
+        }
+
+    }
