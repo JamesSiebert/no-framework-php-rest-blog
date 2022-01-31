64ddf7409f2cf83092807edc04a139825a01689a
James Siebert <js@media360.com.au>
2022-01-31 14:18:15 +1000

    Database config setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # no-framework-php-rest-blog
  PHP REST API with no framework (blog)
+
+
+Reference
+https://youtu.be/OEWXbpUMODk
---
+++ config/Database.php
@@ -0,0 +1,27 @@
+<?php
+    class Database {
+        // DB Params - private to this class only
+        private $host = 'localhost';
+        private $db_name = 'no-framework-php-rest-blog';
+        private $username = 'root';
+        private $password = 'root';
+        private $conn;
+
+        // DB Connect
+        public function connect() {
+            $this->conn = null;
+
+            try {
+                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name,
+                $this->username, $this->password);
+
+                // Set error mode - allows us to get exceptions when we make queries
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            } catch(PDOException $e) {
+                echo 'Connection Error: ' . $e->getMessage();
+            }
+
+            return $this->conn;
+        }
+    }
