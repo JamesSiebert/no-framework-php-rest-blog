7c794b785680e81bf1deb2e059277c9afc5bfb75
James Siebert <js@media360.com.au>
2022-02-01 14:11:15 +1000

    Added Composer + phpdotenv for .env files

--- config/Database.php
+++ config/Database.php
@@ -1,10 +1,19 @@
 <?php
+
+    // Allows the use of .env files
+    declare(strict_types=1);
+    require_once('../../vendor/autoload.php');
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '.env'); // Path: config/.env
+    $dotenv->load();
+    $dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS']);
+
     class Database {
+
         // DB Params - private to this class only
-        private string $host = 'localhost';
-        private string $db_name = 'no_framework_php_rest_blog';
-        private string $username = 'root';
-        private string $password = 'root';
+        // private string $host = 'localhost';
+        // private string $db_name = 'no_framework_php_rest_blog';
+        // private string $username = 'root';
+        // private string $password = 'root';
         private $conn;
 
         // DB Connect
@@ -13,8 +22,11 @@
             $this->conn = null;
 
             try {
-                $this->conn = new PDO(sprintf('mysql:host=%s;dbname=%s', $this->host, $this->db_name),
-                    $this->username, $this->password);
+                $this->conn = new PDO(sprintf('mysql:host=%s;dbname=%s', $_ENV['DB_HOST'], $_ENV['DB_NAME']),
+                    $_ENV['DB_USER'], $_ENV['DB_PASS']);
+
+//                $this->conn = new PDO(sprintf('mysql:host=%s;dbname=%s', $this->host, $this->db_name),
+//                    $this->username, $this->password);
 
                 // Set error mode - allows us to get exceptions when we make queries
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
